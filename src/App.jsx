bc19e3295e43cd3b6c895e9814d09236c7c46ccd
ronlukasteo-crypto <ronlukasteo@gmail.com>
2026-03-02 18:08:35 -0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,51 @@ import Header from './components/Header'
 import Home from './pages/Home'
 import EmployeeMgmt from './pages/EmployeeMgmt'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-
+import { SignedIn, SignedOut, SignInButton, SignOutButton, User, UserDropdown, UserProfile } from '@asgardeo/react'
 
 function App() {
-
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/employee-mgmt" element={<EmployeeMgmt />} />
-      </Routes>
-    </Router>
+    <>
+      <main>
+        <Router>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/employee-mgmt"
+              element={
+                <>
+                  <SignedIn>
+                    <EmployeeMgmt />
+                  </SignedIn>
+                  <SignedOut>
+                    <p>Please sign in to view this page.</p>
+                  </SignedOut>
+                </>
+              }
+            />
+          </Routes>
+        </Router>
+
+        <header>
+          <SignedIn>
+            <UserDropdown />
+            <SignOutButton />
+            <User>
+                {(user) => (
+                  <div>
+                    <p>Welcome back, {user.userName || user.username || user.sub}</p>
+                  </div>
+                )}
+          </User>
+          <UserProfile />
+          </SignedIn>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+        </header>
+      </main>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { AsgardeoProvider } from '@asgardeo/react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <AsgardeoProvider
+      clientId="nfhibnvM3x2k7gehYwdi1fqmpwsa"
+      baseUrl="https://api.asgardeo.io/t/orgron"
+      scopes="openid profile"
+    >
+      <App />
+    </AsgardeoProvider>
+  </StrictMode>
 )
